1292b1b21795207db97a83f26965d563b7b5a7b4
mofuyakoko <asusyako@gmail.com>
2019-04-27 21:30:12 +0900

    controller修正

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,13 +1,21 @@
 package com.example.demo.controller;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@EnableAutoConfiguration
 public class HelloController {
 
-	@GetMapping("/hello")
-	public String getHello() {
+	@RequestMapping("/")
+	public String home() {
 		return "hello";
 	}
+
+//	@GetMapping("/hello")
+//	public String getHello() {
+//		return "hello";
+//	}
+
 }
